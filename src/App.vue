96fd77d3136ab5e4d0be61e3aabb3b3f84c8fa0e
mikemattner <info@mikemattner.com>
2019-03-01 16:27:20 -0500

    Operational ticket generator.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="app">
+    <el-header id="nav">
+          <div class="logo">Numbers</div>
+          <div class="menu">
+            <router-link to="/">Home</router-link>
+            <router-link to="/about">About</router-link>
+          </div>
+    </el-header>
     <el-container>
-      <el-header>
-        <div id="nav">
-          <router-link to="/">Home</router-link> |
-          <router-link to="/about">About</router-link>
-        </div>
-      </el-header>
       <el-main>
-        <router-view/>
+        <transition name="fade" mode="out-in">
+          <router-view/>
+        </transition>
       </el-main>
     </el-container>
   </div>
@@ -17,19 +20,59 @@
 <style lang="scss">
 @import '@/assets/scss/app.scss';
 @import '@/assets/scss/utils/_variables.scss';
-
+body {
+  margin: 0;
+  padding: 0;
+}
 #app {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
 }
+.el-main {
+  min-height: calc( 100vh - 60px );
+  padding: 0;
+  padding-top: 60px !important;
+}
+h1,h2,h3 {
+  color: $darkBlue;
+}
 #nav {
-  padding: 30px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  background-color: $darkBlue;
+  color: $white;
+  font-size: 0.75rem;
+  position: fixed;
+  width: 100%;
+  z-index: 10000;
+
+  .logo {
+    font-size: 1rem;
+    text-transform: uppercase;
+    font-weight: 900;
+  }
   a {
+    color: $white;
+    margin: 0 1rem;
+    text-decoration: none;
     &.router-link-exact-active {
       color: $orange;
-      font-weight: bold;
+      font-weight: 400;
     }
   }
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition-duration: 0.375s;
+  transition-property: all;
+  transition-timing-function: ease-in-out;
+}
+
+.fade-enter,
+.fade-leave-active {
+  opacity: 0;
+  transform: translate(0, 2em);
 }
 </style>
