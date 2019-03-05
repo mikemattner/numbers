c6ad4ff12d2b61ef1b4a19a61e367de78c00c364
mmattner <mmattner@aislerocket.com>
2019-03-05 14:54:15 -0500

    Updating all icons.

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,31 +2,40 @@
   <div class="about">
     <el-row type="flex" class="row-bg" justify="center">
         <el-col :xs="24" :md="16" :lg="12" class="card">
-          <h1>Credits</h1>
-             <p>Numbers is a simple ticket generator for Mega Millions. Built with Vue, 
+            <h1>Credits</h1>
+            <p>Numbers is a simple ticket generator for Mega Millions. Built with Vue, 
              using the Element UI library. Enjoy it, but use it wisely.</p>
-             <dl class="clearfix">
+            <dl>
                 <dt>Concept</dt>
                 <dd>Mike Mattner</dd>
+            </dl>
+            <dl>
                 <dt>Design</dt>
                 <dd>Mike Mattner</dd>
+            </dl>
+            <dl>
                 <dt>Development</dt>
                 <dd>Mike Mattner</dd>
-             </dl>
+            </dl>
              
-             <h2>Version History</h2>
-             <dl class="clearfix">
+            <h2>Version History</h2>
+            <dl>
                 <dt>2.0</dt>
                 <dd>Web application city.</dd>
+            </dl>
+            <dl>
                 <dt>1.5</dt>
                 <dd>Simplified interface and purpose.</dd>
+            </dl>
+            <dl>
                 <dt>1.0</dt>
                 <dd>First release.</dd>
-             </dl>
+            </dl>
              
-             <p class="thanks">Thanks for using <strong>Numbers</strong>!</p>
-           </el-col>
-         </el-row>
+            <p class="thanks">Thank you for using <strong>Numbers</strong>! This webapp might or might 
+            not be updated, and I suggest you gamble wisely.</p>
+        </el-col>
+    </el-row>
   </div>
 </template>
 
@@ -45,7 +54,8 @@ export default {
         margin: 0 0 1rem;
     }
     dl {
-        margin: 0;
+        margin: 0 0 1rem;
+        
         dt {
             font-weight: bold; 
             color: $midGreen;
@@ -53,7 +63,6 @@ export default {
             border-top: 1px solid rgba(255, 255, 255, 0.05);
         }
         dd {
-            padding: 0 0 0.5rem; 
             margin: 0; 
         }
     }
