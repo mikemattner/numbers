ab8a12cf4fbd204e49cc6c3fd2a94218af758383
mmattner <mmattner@aislerocket.com>
2019-03-07 13:40:45 -0500

    Adding headroom to app.

--- src/components/HeaderBar.vue
+++ src/components/HeaderBar.vue
@@ -1,42 +1,53 @@
 <template>
-    <el-header id="nav">
-      <div class="logo">
-          <span>Numbers</span>
-          <i class="el-icon-more"></i>
-      </div>
-    </el-header>
+    <headroom>
+      <header class="headerbar">
+        <div class="logo">
+            <span>Numbers</span>
+            <i class="el-icon-more"></i>
+        </div>
+      </header>
+    </headroom>
 </template>
 
 <script>
+import { headroom } from 'vue-headroom'
 export default {
   name: 'HeaderBar',
+  components: {
+      headroom
+  }
 }
 </script>
 
 <style scoped lang="scss">
 @import '@/assets/scss/utils/_variables.scss';
+.headroom {
 
-#nav {
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  background-color: $white;
-  color: $midGreen;
-  font-size: 0.75rem;
-  position: fixed;
-  width: 100%;
-  z-index: 10000;
-  box-shadow: 0 2px 15px rgba(0,0,0,0.1);
-  
-  .logo {
-    font-size: 1.5rem;
-    font-weight: 700;
+  .headerbar {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    background-color: $white;
+    color: $midGreen;
+    font-size: 0.75rem;
+    height: 60px;
+    position: fixed;
+    width: 100%;
+    z-index: 10000;
+    box-shadow: 0 2px 15px rgba(0,0,0,0.1);
+    transition: all 0.25s ease-in-out;
+    padding: 0 20px;
+
+    .logo {
+      font-size: 1.5rem;
+      font-weight: 700;
       display: flex;
       justify-content: space-between;
       align-items: center;
       letter-spacing: -0.5px;
       width: 100%;
       color: $black;
+      transition: all 0.25s ease-in-out;
 
       i {
         color: $white;
@@ -44,30 +55,44 @@ export default {
         margin-right: 0.5rem;
         font-size: 2rem;
         border-radius: 4px;
+        transition: all 0.25s ease-in-out;
+
         &:before {
           display: block;
           transform: rotate(45deg);
         }
-     }
-    .brand-logo__image {
-      width: 30px;
-      height: 30px;
-      margin-right: 0.5rem;
-      .circle-shape {
-        fill: $midGreen;
-      }
-      .logo-shape {
-        fill: $white;
+       }
+    }
+    a {
+      color: $black;
+      margin: 0 1rem;
+      text-decoration: none;
+      letter-spacing: 0.065em;
+      &.router-link-exact-active {
+        color: $black;
       }
     }
   }
-  a {
-    color: $black;
-    margin: 0 1rem;
-    text-decoration: none;
-    letter-spacing: 0.065em;
-    &.router-link-exact-active {
-      color: $black;
+  &--not-top {
+    .headerbar {  
+      height: 40px;
+      .logo {
+        font-size: 1rem;
+
+        i {
+          font-size: 1.25rem;
+        }
+      }
+    }
+  }
+  &--pinned {
+    .headerbar { 
+      top: 0;
+    }
+  }
+  &--unpinned {
+    .headerbar { 
+      top: -40px;
     }
   }
 }
